5da4c9d41a11e292651dce04305d3a8f648a1843
Dagner Chuman <dagner_smooth@outlook.com>
2023-04-03 22:58:45 -0500

    mi primer proyecto

---
+++ src/test/java/com/sistemaventas/chuman/sistemaventas/SistemaventasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemaventas.chuman.sistemaventas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaventasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
