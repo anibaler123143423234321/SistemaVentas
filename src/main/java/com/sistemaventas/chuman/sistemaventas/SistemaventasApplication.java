6c8c44ce3072b4b20d345aa8e99125c4456c8473
lfuenmed <luiguialexander.fuentesmedina@everis.nttdata.com>
2023-04-06 00:37:55 -0500

    new changes by Alexander v5

--- src/main/java/com/sistemaventas/chuman/sistemaventas/SistemaventasApplication.java
+++ src/main/java/com/sistemaventas/chuman/sistemaventas/SistemaventasApplication.java
@@ -2,8 +2,10 @@ package com.sistemaventas.chuman.sistemaventas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(scanBasePackages = {"mapper"})
+@SpringBootApplication
+@ComponentScan("com.sistemaventas.chuman.sistemaventas.mapper")
 public class SistemaventasApplication {
 
 	public static void main(String[] args) {
