601b9ab891f93a437f0de2c47d2502fac19786e8
Dagner <clluendagnerani@crece.uss.edu.pe>
2023-06-02 11:22:50 -0500

    tercer avance

--- src/main/java/com/sistemaventas/chuman/sistemaventas/controller/UsuarioController.java
+++ src/main/java/com/sistemaventas/chuman/sistemaventas/controller/UsuarioController.java
@@ -3,9 +3,11 @@ package com.sistemaventas.chuman.sistemaventas.controller;
 import com.sistemaventas.chuman.sistemaventas.entity.Usuario;
 import com.sistemaventas.chuman.sistemaventas.service.UsuarioService;
 import com.sistemaventas.chuman.sistemaventas.utils.GenericResponse;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.awt.*;
 
 @RestController
 @RequestMapping("api/usuario")
@@ -15,7 +17,7 @@ public class UsuarioController {
     public UsuarioController(UsuarioService service) {
         this.service = service;
     }
-    @PostMapping("/login")
+    @RequestMapping(value="/login", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
     public GenericResponse<Usuario> login(HttpServletRequest request){
         String email = request.getParameter("email");
         String contrasenia = request.getParameter("pass");
